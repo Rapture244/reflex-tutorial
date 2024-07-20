d2e86f568d13b2aa666c77fb9819cfa51506206e
Ovidiu PASCAL <pascal.ovidiuu@gmail.com>
2024-07-20 20:13:56 +0200

    HTML inputs for dynamic content changes
    What did I do:
    - Added a rx.input component
    - Created an event_handler to handle changes in the input

--- reflex_tutorial/reflex_tutorial.py
+++ reflex_tutorial/reflex_tutorial.py
@@ -9,19 +9,13 @@ from rxconfig import config
 class State(rx.State):
     """The app state."""
 
-    # STEP 3: Create 3 variables with the 1st one being our title heading
     title_heading = "Welcome to Reflex!"
-    heading1 = "All CAPS when u spell the man name"
-    heading2 = "MF DOOM - The Supervillain"
 
-    # STEP 4: Create an even handler that that changes the value of the var 'title_heading'
-    def change_heading(self):
-        if self.title_heading == "Welcome to Reflex!":
-            self.title_heading = self.heading1
-        elif self.title_heading == self.heading1:
-            self.title_heading = self.heading2
-        else:
-            self.title_heading = "Welcome to Reflex!"
+    # STEP 2: Defined an event_handler
+    def handle_title_input_change(self, val):
+        self.title_heading = val
+
+
 
     ...
 
@@ -33,7 +27,7 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading(State.title_heading, size="9"),  # STEP 2: turn the heading string into a variable that we move into our State
+            rx.heading(State.title_heading, size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
@@ -45,13 +39,17 @@ def index() -> rx.Component:
                 href="https://reflex.dev/docs/getting-started/introduction/",
                 is_external=True,
             ),
+            # STEP 1: added a component input that changes the title
+            rx.input(placeholder =State.title_heading,
+                     default_value = State.title_heading,
+                     max_value = 30,
+                     on_change = State.handle_title_input_change,
+                     ),
 
-            # STEP 1: Create a new button
             rx.button(
                 rx.icon(tag="skull"),
-                "Click me to see what I can do!",
+                "I'm just a cool Button but i do nothing!",
                 color_scheme="red",
-                on_click=State.change_heading, # STEP 5 : We add our event_trigger = Event_handler to our component
             ),
 
             spacing="5",
