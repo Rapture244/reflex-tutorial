5cbe012c7b7bfa6afbc82c8c38d08f90e7b26914
Ovidiu PASCAL <pascal.ovidiuu@gmail.com>
2024-07-21 13:40:21 +0200

    Better code management
    What did we do ?
    - create a package 'ui'
    - add 2 modules called 'base' & 'nav'
    - Respectively contain the 'base_page' & the  'nav_bar' functions
    - import the 'base_page' function inside the project pyton file

--- reflex_tutorial/reflex_tutorial.py
+++ reflex_tutorial/reflex_tutorial.py
@@ -1,9 +1,9 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
-
 from rxconfig import config
 
+from .ui.base import base_page # from the package 'ui' grab the module 'base' and import the function 'base_page'. Th '.' means in the same root as the current file
 
 #----------------------------------------------------- BACKEND ------------------------------------------------------------------------------------
 class State(rx.State):
@@ -19,108 +19,6 @@ class State(rx.State):
     ...
 
 #----------------------------------------------------- FRONTEND -----------------------------------------------------------------------------------
-def navbar_link(text: str, url: str) -> rx.Component:
-    return rx.link(
-        rx.text(text, size="4", weight="medium"), href=url
-    )
-
-
-def navbar() -> rx.Component:
-    return rx.box(
-        rx.desktop_only(
-            rx.hstack(
-                rx.hstack(
-                    rx.image(
-                        src="/logo.jpg",
-                        width="2.25em",
-                        height="auto",
-                        border_radius="25%",
-                    ),
-                    rx.heading(
-                        "Reflex", size="7", weight="bold"
-                    ),
-                    align_items="center",
-                ),
-                rx.hstack(
-                    navbar_link("Home", "/#"),
-                    navbar_link("About", "/#"),
-                    navbar_link("Pricing", "/#"),
-                    navbar_link("Contact", "/#"),
-                    spacing="5",
-                ),
-                rx.hstack(
-                    rx.button(
-                        "Sign Up",
-                        size="3",
-                        variant="outline",
-                    ),
-                    rx.button("Log In", size="3"),
-                    spacing="4",
-                    justify="end",
-                ),
-                justify="between",
-                align_items="center",
-            ),
-        ),
-        rx.mobile_and_tablet(
-            rx.hstack(
-                rx.hstack(
-                    rx.image(
-                        src="/logo.jpg",
-                        width="2em",
-                        height="auto",
-                        border_radius="25%",
-                    ),
-                    rx.heading(
-                        "Reflex", size="6", weight="bold"
-                    ),
-                    align_items="center",
-                ),
-                rx.menu.root(
-                    rx.menu.trigger(
-                        rx.icon("menu", size=30)
-                    ),
-                    rx.menu.content(
-                        rx.menu.item("Home"),
-                        rx.menu.item("About"),
-                        rx.menu.item("Pricing"),
-                        rx.menu.item("Contact"),
-                        rx.menu.separator(),
-                        rx.menu.item("Log in"),
-                        rx.menu.item("Sign up"),
-                    ),
-                    justify="end",
-                ),
-                justify="between",
-                align_items="center",
-            ),
-        ),
-        bg=rx.color("accent", 3),
-        padding="1em",
-        # position="fixed",
-        # top="0px",
-        # z_index="5",
-        width="100%",
-    )
-
-
-
-def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
-    if not isinstance(child, rx.Component):
-        child = rx.heading("This is not a valid child element")
-    if hide_navbar:
-        return rx.container(
-            child,
-            rx.logo(),
-            rx.color_mode.button(position="bottom_left"),
-        )
-    return rx.container(
-        navbar(),
-        child,
-        rx.logo(),
-        rx.color_mode.button(position="top-right"),
-    )
-
 def index() -> rx.Component:
     # Welcome Page (Index)
     return base_page(
@@ -157,6 +55,3 @@ app = rx.App()
 app.add_page(index)
 
 
-#----------------------------------------------------- WHAT DID I DO ? -----------------------------------------------------------------------------
-""" 
-"""
