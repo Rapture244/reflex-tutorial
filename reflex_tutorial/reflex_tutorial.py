668190157febbaedea96c0c6c0ec4e2ff3c9888e
Ovidiu PASCAL <pascal.ovidiuu@gmail.com>
2024-07-21 12:47:51 +0200

    Building a custom Reflex component

--- reflex_tutorial/reflex_tutorial.py
+++ reflex_tutorial/reflex_tutorial.py
@@ -11,7 +11,6 @@ class State(rx.State):
 
     title_heading = "Welcome to Reflex!"
 
-    # STEP 2: Defined an event_handler
     def handle_title_input_change(self, val):
         self.title_heading = val
 
@@ -19,13 +18,29 @@ class State(rx.State):
 
     ...
 
-
-
 #----------------------------------------------------- FRONTEND -----------------------------------------------------------------------------------
+def navbar() -> rx.Component:
+    return rx.heading("SaaS", size="9")
+
+def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
+    if not isinstance(child, rx.Component):
+        child = rx.heading("This is not a valid child element")
+    if hide_navbar:
+        return rx.container(
+            child,
+            rx.logo(),
+            rx.color_mode.button(position="bottom_left"),
+        )
+    return rx.container(
+        navbar(),
+        child,
+        rx.logo(),
+        rx.color_mode.button(position="bottom_left"),
+    )
+
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
+    return base_page(
         rx.vstack(
             rx.heading(State.title_heading, size="9"),
             rx.text(
@@ -33,34 +48,33 @@ def index() -> rx.Component:
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
-
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
                 is_external=True,
             ),
-            # STEP 1: added a component input that changes the title
             rx.input(placeholder =State.title_heading,
-                     default_value = State.title_heading,
-                     max_value = 30,
-                     on_change = State.handle_title_input_change,
-                     ),
-
+                    default_value = State.title_heading,
+                    max_value = 30,
+                    on_change = State.handle_title_input_change,
+            ),
             rx.button(
                 rx.icon(tag="skull"),
                 "I'm just a cool Button but i do nothing!",
                 color_scheme="red",
             ),
-
             spacing="5",
             justify="center",
             min_height="85vh",
-
         ),
-
-        rx.logo(),
+        #hide_navbar=True
     )
 
 #----------------------------------------------------- PAGES --------------------------------------------------------------------------------------
 app = rx.App()
 app.add_page(index)
+
+
+#----------------------------------------------------- WHAT DID I DO ? -----------------------------------------------------------------------------
+""" 
+"""
